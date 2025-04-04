dd54b20c259ea29448fd85de7efa63e193584616
Enzo  Vonkunoschy <42283589+EnzoVonkunoschy@users.noreply.github.com>
2025-04-04 17:55:54 -0300

    Update clases.js
    Borré clase repetida

--- clases.js
+++ clases.js
@@ -7,29 +7,6 @@ class Turno {
     }
 }
 
-
-module.exports = { Turno };
-
-
-
-
-class Turno {
-    constructor(dia, turno, libre) {
-        this.dia = dia
-        this.turno = turno
-        this.libre = libre
-    }
-
-    // mostrarTurno(){
-    //     console.log(`Dia: ${this.dia} Hora: ${this.hora} libre: ${this.libre}`)
-    // }
-}
-
-// const miTurno = new Turno("lunes",2,true)
-
-// miTurno.mostrarTurno();
-
-
 class Cliente{
     constructor(nombre,dni,telefono){
         this.nombre = nombre
